e598adfaa186fd0a2cfbb8950f2702e8341c89cf
nurmdrafi <nurmdrafi2014@gmail.com>
2022-02-28 22:43:37 +0600

    show player details

--- app.js
+++ app.js
@@ -1,35 +1,68 @@
-const loadData = () =>{
-    const searchField = document.getElementById('search-box');
-    const searchText = searchField.value;
-    const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchText}`
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displayData(data.player))
-}
+const loadPlayers = () => {
+  const searchField = document.getElementById("search-box");
+  const searchText = searchField.value;
+  searchField.value = "";
+  const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchText}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayPlayers(data.player));
+};
 
-const displayData = (players) =>{
-    for(const player of players){
-        // console.log(player);
-        const {strPlayer, strNationality, strHeight, strThumb, strDescriptionEN} = player;
-        console.log(strPlayer, strNationality, strHeight);
+const displayPlayers = (players) => {
+  for (const player of players) {
+    const {
+      idPlayer,
+      strPlayer,
+      strNationality,
+      strHeight,
+      strThumb,
+      strDescriptionEN,
+    } = player;
 
-        // players container
-        const playersContainer = document.getElementById('players-container');
-        const div = document.createElement("div");
-        div.innerHTML = `
+    // players container
+    const playersContainer = document.getElementById("players-container");
+
+    const div = document.createElement("div");
+    div.innerHTML = `
         <div class="col">
                     <div class="card">
-                        <img src="${strThumb}" class="card-img-top" alt="..." style="height: 300px;>
-                        <div class="card-body">
-                            <h3 class="card-title">${strPlayer}</h3>
-                            <h4>${strNationality}</h4>
+                        <img src="${strThumb}" class="card-img-top w-50 mx-auto" alt="...">
+                        <div class="card-body text-center">
+                            <h3 class="card-title">Name: ${strPlayer}</h3>
+                            <h4>Country: ${strNationality}</h4>
                             <h5>${strHeight}</h5>
-                            <p class="card-text">${strDescriptionEN.slice(1, 200)}</p>
-                            <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Details</a>
+                            <p class="card-text">${strDescriptionEN.slice(
+                              1,
+                              200
+                            )}</p>
+                            <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#player-details"  onclick="loadPlayerDetails('${idPlayer}')">Details</a>
                         </div>       
                     </div>
                 </div>
-        `
-        playersContainer.appendChild(div);
-    }
-}
+        `;
+    // playersContainer.innerHTML = "";
+    playersContainer.appendChild(div);
+  }
+};
+
+const loadPlayerDetails = (idPlayer) => {
+    console.log(idPlayer);
+  // Player Details
+  const playerDetailsUrl = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${idPlayer}`;
+  fetch(playerDetailsUrl)
+    .then((res) => res.json())
+    // .then((data) => console.log(data));
+    .then((data) => showPlayerDetails(data.players[0]));
+};
+
+// Show Details
+
+const showPlayerDetails = (data) => {
+  const details = document.getElementById("details");
+  const playerDetails = document.createElement("div");
+  playerDetails.innerHTML = `
+    <h1>${data.strPlayer}</h1>
+    <h2>${data.strNationality}</h2>
+`;
+  details.appendChild(playerDetails);
+};
