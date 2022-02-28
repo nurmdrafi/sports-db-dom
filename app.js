4c4ae6f18080fad1f3213f06482716a4d7555970
nurmdrafi <nurmdrafi2014@gmail.com>
2022-02-28 21:12:00 +0600

    data loaded

--- app.js
+++ app.js
@@ -10,8 +10,26 @@ const loadData = () =>{
 const displayData = (players) =>{
     for(const player of players){
         // console.log(player);
-        const {strPlayer, strNationality, strHeight} = player;
+        const {strPlayer, strNationality, strHeight, strThumb, strDescriptionEN} = player;
         console.log(strPlayer, strNationality, strHeight);
-        
+
+        // players container
+        const playersContainer = document.getElementById('players-container');
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <div class="col">
+                    <div class="card">
+                        <img src="${strThumb}" class="card-img-top" alt="..." style="height: 300px;>
+                        <div class="card-body">
+                            <h3 class="card-title">${strPlayer}</h3>
+                            <h4>${strNationality}</h4>
+                            <h5>${strHeight}</h5>
+                            <p class="card-text">${strDescriptionEN.slice(1, 200)}</p>
+                            <a href="#" class="btn btn-primary">Details</a>
+                        </div>
+                    </div>
+                </div>
+        `
+        playersContainer.appendChild(div);
     }
 }
